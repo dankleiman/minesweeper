c02ef2785bb88886a16da6305f034ec605cc9518
Dan Kleiman <info@dankleiman.com>
2014-06-11 13:37:37 -0400

    how to clear cells

--- minefield.rb
+++ minefield.rb
@@ -6,30 +6,54 @@ class Minefield
     @column_count = column_count
     @row_count = row_count
     @mine_count = mine_count
-    @detenated = false
+    @detonated = false
     @mines = place_mines
+    @cleared_cells = []
   end
 
   # Return true if the cell been uncovered, false otherwise.
   def cell_cleared?(row, col)
-    false
+    if @cleared_cells.include?([row,col])
+      true
+    else
+      false
+    end
   end
 
   # Uncover the given cell. If there are no adjacent mines to this cell
   # it should also clear any adjacent cells as well. This is the action
   # when the player clicks on the cell.
   def clear(row, col)
-    if contains_mine?(row, col)
-      @detenated = true
+    if !cell_cleared?(row,col)
+      @cleared_cells << [row,col]
+      if contains_mine?(row, col)
+        @detonated = true
+      elsif adjacent_mines(row, col) == 0
+        (-1..1).each do |i|
+          adj_row = row + i
+          (-1..1).each do |j|
+            adj_col = col + j
+            if i != 0 || j != 0
+              clear(adj_row,adj_col) unless over_the_edge?(adj_row, adj_col)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def over_the_edge?(row, col)
+    if row < 0 || row > @row_count || col < 0 || col > @column_count
+      true
     else
-      adjacent_mines(row, col)
+      false
     end
   end
 
   # Check if any cells have been uncovered that also contained a mine. This is
   # the condition used to see if the player has lost the game.
   def any_mines_detonated?
-    if @detenated
+    if @detonated
       true
     else
       false
